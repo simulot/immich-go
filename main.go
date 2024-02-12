4e0c63f28ef74044c75a93282ae06443434b5487
simulot <jfcassan+github@gmail.com>
2024-02-12 18:44:00 +0100

    refactore stack command

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/simulot/immich-go/cmd"
 	"github.com/simulot/immich-go/cmd/duplicate"
 	"github.com/simulot/immich-go/cmd/metadata"
+	"github.com/simulot/immich-go/cmd/stack"
 	"github.com/simulot/immich-go/cmd/upload"
 	"github.com/simulot/immich-go/logger"
 )
@@ -80,8 +81,8 @@ func Run(ctx context.Context) error {
 		err = duplicate.DuplicateCommand(ctx, &app, fs.Args()[1:])
 	case "metadata":
 		err = metadata.MetadataCommand(ctx, &app, fs.Args()[1:])
-	// case "stack":
-	// 	err = stack.NewStackCommand(ctx, app.Immich, app.Logger, fs.Args()[1:])
+	case "stack":
+		err = stack.NewStackCommand(ctx, &app, fs.Args()[1:])
 	// case "tool":
 	// 	err = tool.CommandTool(ctx, app.Immich, app.Logger, fs.Args()[1:])
 	default:
