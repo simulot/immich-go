37f4b830f46b57186a807c192ef475fb5cabaeea
Jean François CASSAN <simulot@users.noreply.github.com>
2024-05-20 18:57:00 +0200

    Use XDG_CONFIG_HOME for storing config (#248)
    Fixes #215

--- helpers/configuration/config.go
+++ helpers/configuration/config.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
+	"time"
 )
 
 type Configuration struct {
@@ -12,18 +13,20 @@ type Configuration struct {
 	APIKey    string
 }
 
-// DefaultFile return the default configuration file name
-// Return a local file nama when the default UserHomeDir can't be determined,
-func DefaultFile() string {
-	d, err := os.UserHomeDir()
+// DefaultConfigFile return the default configuration file name
+// Return a local file when the default UserHomeDir can't be determined,
+func DefaultConfigFile() string {
+	config, err := os.UserConfigDir()
 	if err != nil {
-		return "immich-go.json"
+		// $XDG_CONFIG_HOME nor $HOME is set
+		// return current
+		return "./immich-go.json"
 	}
-	return filepath.Join(d, ".immich-go", "immich-go.json")
+	return filepath.Join(config, "immich-go", "immich-go.json")
 }
 
-// Read the configuration in file name
-func Read(name string) (Configuration, error) {
+// ConfigRead the configuration in file name
+func ConfigRead(name string) (Configuration, error) {
 	f, err := os.Open(name)
 	if err != nil {
 		return Configuration{}, err
@@ -55,4 +58,21 @@ func (c Configuration) Write(name string) error {
 	enc := json.NewEncoder(f)
 	enc.SetIndent("", "  ")
 	return enc.Encode(c)
+}
+
+// DefaultLogDir give the default log file
+// Return the current dir when $HOME not $XDG_CACHE_HOME are not set
+func DefaultLogFile() string {
+	f := time.Now().Format("immich-go_2006-01-02_15-04-05.log")
+	d, err := os.UserCacheDir()
+	if err != nil {
+		return f
+	}
+	return filepath.Join(d, "immich-go", f)
+}
+
+// MakeDirForFile create all dirs to write the given file
+func MakeDirForFile(f string) error {
+	dir := filepath.Dir(f)
+	return os.MkdirAll(dir, 0o700)
 }
