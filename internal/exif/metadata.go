5b15c6cf439daa6661ebf206d3ea23d644b13fec
simulot <jfcassan+github@gmail.com>
2024-11-30 08:27:02 +0100

    refactor: remove the support of exiftool

--- internal/exif/metadata.go
+++ internal/exif/metadata.go
@@ -2,25 +2,23 @@ package exif
 
 import (
 	"io"
+	"time"
 
 	"github.com/simulot/immich-go/internal/assets"
 )
 
 // GetMetaData read metadata from the asset file to  enrich the metadata structure
-func GetMetaData(r io.Reader, name string, options ExifToolFlags) (*assets.Metadata, error) {
-	if options.UseExifTool && options.et != nil {
-		return MetadataFromExiftool(r, name, options)
-	}
-	return MetadataFromDirectRead(r, name, options.Timezone.TZ)
+func GetMetaData(r io.Reader, name string, local *time.Location) (*assets.Metadata, error) {
+	return MetadataFromDirectRead(r, name, local)
 }
 
 // MetadataFromExiftool call exiftool to get exif data
-func MetadataFromExiftool(f io.Reader, name string, options ExifToolFlags) (*assets.Metadata, error) {
-	// be sure the file is completely extracted in the temp file
-	_, err := io.Copy(io.Discard, f)
-	if err != nil {
-		return nil, err
-	}
+// func MetadataFromExiftool(f io.Reader, name string, options ExifToolFlags) (*assets.Metadata, error) {
+// 	// be sure the file is completely extracted in the temp file
+// 	_, err := io.Copy(io.Discard, f)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return options.et.ReadMetaData(name)
-}
+// 	return options.et.ReadMetaData(name)
+// }
