07333b4da2d64be773ab3137fd16f4d64b5cec2d
simulot <jfcassan+github@gmail.com>
2024-12-29 18:23:31 +0100

    refactor: consolidate environment initialization and reset logic in e2e tests

---
+++ internal/e2eTests/e2e/e2e.go
@@ -0,0 +1,63 @@
+package e2e
+
+import (
+	"fmt"
+	"os/exec"
+	"testing"
+
+	"github.com/joho/godotenv"
+)
+
+var myEnv map[string]string
+
+const e2eEnv = "../../../e2e.env"
+
+func InitMyEnv() {
+	if len(myEnv) > 0 {
+		return
+	}
+	var err error
+	e, err := godotenv.Read(e2eEnv)
+	if err != nil {
+		panic(fmt.Sprintf("cant initialize environment variables: %s", err))
+	}
+	myEnv = e
+	if myEnv["IMMICHGO_TESTFILES"] == "" {
+		panic("missing IMMICHGO_TESTFILES in .env file")
+	}
+}
+
+func MyEnv(key string) string {
+	if len(myEnv) == 0 {
+		InitMyEnv()
+	}
+	return myEnv[key]
+}
+
+func ResetImmich(t *testing.T) {
+	// Reset immich's database
+	// https://github.com/immich-app/immich/blob/main/e2e/src/utils.ts
+	//
+	c := exec.Command("docker", "exec", "-i", "immich_postgres", "psql", "--dbname=immich", "--username=postgres", "-c",
+		`
+		DELETE FROM asset_stack CASCADE;
+		DELETE FROM libraries CASCADE;
+		DELETE FROM shared_links CASCADE;
+		DELETE FROM person CASCADE;
+		DELETE FROM albums CASCADE;
+		DELETE FROM assets CASCADE;
+		DELETE FROM asset_faces CASCADE;
+		DELETE FROM activity CASCADE;
+		--DELETE FROM api_keys CASCADE;
+		--DELETE FROM sessions CASCADE;
+		--DELETE FROM users CASCADE;
+		DELETE FROM "system_metadata" where "key" NOT IN ('reverse-geocoding-state', 'system-flags');
+		DELETE FROM tags CASCADE;
+		`,
+	)
+	b, err := c.CombinedOutput()
+	if err != nil {
+		t.Log(string(b))
+		t.Fatal(err)
+	}
+}
