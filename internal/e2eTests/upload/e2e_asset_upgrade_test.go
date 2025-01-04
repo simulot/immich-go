86c2a9c1d8609ce5274630ca34ee396f2de0c5d8
simulot <jfcassan+github@gmail.com>
2025-01-04 10:00:46 +0100

    Enhance end-to-end tests for asset upgrades and improve album verification logic

--- internal/e2eTests/upload/e2e_asset_upgrade_test.go
+++ internal/e2eTests/upload/e2e_asset_upgrade_test.go
@@ -59,7 +59,7 @@ func TestUpgradePhotoFolder(t *testing.T) {
 		t.Fatal("Asset not found")
 	}
 
-	AssetID := assets[0].ID // Keep the ID for further tests
+	AssetID := assets[0].ID // Keep the ID for further checks
 
 	c, a = cmd.RootImmichGoCommand(ctx)
 	c.SetArgs([]string{
@@ -94,16 +94,6 @@ func TestUpgradePhotoFolder(t *testing.T) {
 }
 
 func TestUpgradeGooglePhotoFolder(t *testing.T) {
-	fsys := os.DirFS("../../../app/cmd/upload/TEST_DATA")
-	lowSHA1, err := hash.Base64Encode(hash.FileSHA1Hash(fsys, "folder/low/PXL_20231006_063000139.jpg"))
-	if err != nil {
-		t.Fatal(err)
-	}
-	highSHA1, err := hash.Base64Encode(hash.FileSHA1Hash(fsys, "Takeout1/Google Photos/Album test 6-10-23/PXL_20231006_063000139.jpg"))
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	e2e.InitMyEnv()
 	e2e.ResetImmich(t)
 	client, err := e2e.GetImmichClient()
@@ -131,6 +121,17 @@ func TestUpgradeGooglePhotoFolder(t *testing.T) {
 
 	e2e.WaitingForJobsEnding(ctx, client, t)
 
+	assets, err := client.GetAssetsByImageName(ctx, "PXL_20231006_063000139.jpg")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	if len(assets) != 1 {
+		t.Fatal("Asset not found")
+	}
+
+	AssetID := assets[0].ID // Keep the ID for further checks
+
 	c, a = cmd.RootImmichGoCommand(ctx)
 	c.SetArgs([]string{
 		"upload", "from-google-photos",
@@ -147,20 +148,81 @@ func TestUpgradeGooglePhotoFolder(t *testing.T) {
 	}
 
 	e2e.WaitingForJobsEnding(ctx, client, t)
-	assets, err := client.GetAssetsByHash(ctx, lowSHA1)
+
+	al, err := client.GetAssetAlbums(ctx, AssetID)
 	if err != nil {
 		t.Fatal(err)
 		return
 	}
-	if len(assets) != 0 {
-		t.Errorf("Low Asset still present")
+
+	if len(al) != 2 {
+		t.Errorf("Expecting 3 albums, got %d", len(al))
 	}
-	assets, err = client.GetAssetsByHash(ctx, highSHA1)
+}
+
+func TestUpgradeGooglePhotoFolder2(t *testing.T) {
+	e2e.InitMyEnv()
+	e2e.ResetImmich(t)
+	client, err := e2e.GetImmichClient()
 	if err != nil {
 		t.Fatal(err)
 		return
 	}
-	if len(assets) != 0 {
-		t.Errorf("High Asset not present")
+
+	ctx := context.Background()
+
+	c, a := cmd.RootImmichGoCommand(ctx)
+	c.SetArgs([]string{
+		"upload", "from-google-photos",
+		"--server=" + e2e.MyEnv("IMMICHGO_SERVER"),
+		"--api-key=" + e2e.MyEnv("IMMICHGO_APIKEY"),
+		"--api-trace",
+		"../../../app/cmd/upload/TEST_DATA/Takeout1",
+	})
+	err = c.ExecuteContext(ctx)
+	if err != nil && a.Log().GetSLog() != nil {
+		a.Log().Error(err.Error())
+		t.Fatal(err)
+		return
+	}
+
+	e2e.WaitingForJobsEnding(ctx, client, t)
+
+	c, a = cmd.RootImmichGoCommand(ctx)
+	c.SetArgs([]string{
+		"upload", "from-folder",
+		"--server=" + e2e.MyEnv("IMMICHGO_SERVER"),
+		"--api-key=" + e2e.MyEnv("IMMICHGO_APIKEY"),
+		"--folder-as-album=FOLDER",
+		"../../../app/cmd/upload/TEST_DATA/folder/high",
+	})
+	err = c.ExecuteContext(ctx)
+	if err != nil && a.Log().GetSLog() != nil {
+		a.Log().Error(err.Error())
+		t.Fatal(err)
+		return
+	}
+
+	e2e.WaitingForJobsEnding(ctx, client, t)
+
+	assets, err := client.GetAssetsByImageName(ctx, "PXL_20231006_063000139.jpg")
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	if len(assets) != 2 { // 1 visible, 1 trashed (low quality)
+		t.Fatal("Asset not found")
+	}
+
+	AssetID := assets[0].ID // Keep the ID for further checks
+
+	al, err := client.GetAssetAlbums(ctx, AssetID)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	if len(al) != 2 {
+		t.Errorf("Expecting asset in 3 albums, got %d", len(al))
 	}
 }
