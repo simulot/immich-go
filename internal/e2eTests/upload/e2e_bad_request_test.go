bc3a84fa609e1e9cf63b443520dada6148d7e197
simulot <jfcassan+github@gmail.com>
2025-01-04 16:43:30 +0100

    Add test data for Google Photos upload and enhance bad request handling

--- internal/e2eTests/upload/e2e_bad_request_test.go
+++ internal/e2eTests/upload/e2e_bad_request_test.go
@@ -2,9 +2,12 @@ package upload
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/simulot/immich-go/app/cmd"
+	"github.com/simulot/immich-go/immich"
 	"github.com/simulot/immich-go/internal/e2eTests/e2e"
 )
 
@@ -17,17 +20,57 @@ func Test_BadRequest(t *testing.T) {
 
 	c, a := cmd.RootImmichGoCommand(ctx)
 	c.SetArgs([]string{
+		"upload", "from-folder",
+		"--server=" + e2e.MyEnv("IMMICHGO_SERVER"),
+		"--api-key=" + e2e.MyEnv("IMMICHGO_APIKEY"),
+		"--no-ui",
+		"--api-trace",
+		"TEST_DATA/fixtures",
+	})
+
+	err := c.ExecuteContext(ctx)
+	if err != nil && a.Log().GetSLog() != nil {
+		a.Log().Error(err.Error())
+		t.Fatal(err)
+	}
+
+	client, err := immich.NewImmichClient(
+		e2e.MyEnv("IMMICHGO_SERVER"),
+		e2e.MyEnv("IMMICHGO_APIKEY"),
+	)
+	ctx2, cancel := context.WithDeadline(ctx, time.Now().Add(30*time.Second))
+check:
+	for {
+		select {
+		case <-ctx2.Done():
+			t.Fatal("Timeout waiting for metadata job to terminate")
+		default:
+			jobs, err := client.GetJobs(ctx2)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if jobs["metadataExtraction"].JobCounts.Active == 0 {
+				cancel()
+				break check
+			}
+			fmt.Println("Waiting for metadata extraction to finish")
+			time.Sleep(1 * time.Second)
+		}
+	}
+
+	c, a = cmd.RootImmichGoCommand(ctx)
+	c.SetArgs([]string{
 		"upload", "from-google-photos",
 		"--server=" + e2e.MyEnv("IMMICHGO_SERVER"),
 		"--api-key=" + e2e.MyEnv("IMMICHGO_APIKEY"),
 		"--no-ui",
 		"--api-trace",
-		e2e.MyEnv("IMMICHGO_TESTFILES") + "/bad request/Google_Photos",
+		"TEST_DATA/takeout",
 	})
 
-	// let's start
-	err := c.ExecuteContext(ctx)
+	err = c.ExecuteContext(ctx)
 	if err != nil && a.Log().GetSLog() != nil {
 		a.Log().Error(err.Error())
+		t.Fatal(err)
 	}
 }
