28cccfdfc82782d6492878694f545a5899bc063d
simulot <jfcassan+github@gmail.com>
2025-01-04 17:28:42 +0100

    Add test for handling bad requests and improve logging in e2e tests

--- internal/e2eTests/upload/e2e_bad_request_test.go
+++ internal/e2eTests/upload/e2e_bad_request_test.go
@@ -2,17 +2,50 @@ package upload
 
 import (
 	"context"
-	"fmt"
+	"os"
 	"testing"
-	"time"
 
 	"github.com/simulot/immich-go/app/cmd"
-	"github.com/simulot/immich-go/immich"
 	"github.com/simulot/immich-go/internal/e2eTests/e2e"
 )
 
+func Test_BadRequestOnPurpose(t *testing.T) {
+	client, err := e2e.GetImmichClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logfileName := os.TempDir() + "/immich.log"
+	logfile, err := os.Create(logfileName)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+	defer logfile.Close()
+
+	client.EnableAppTrace(logfile)
+	u, err := client.ValidateConnection(context.Background())
+	_ = u
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	err = client.DeleteAssets(context.Background(), []string{"bad-request"}, false)
+	if err == nil {
+		t.Fatal("Error expected")
+		return
+	}
+}
+
 func Test_BadRequest(t *testing.T) {
 	t.Log("Test_BadRequest")
+
+	client, err := e2e.GetImmichClient()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	e2e.InitMyEnv()
 	e2e.ResetImmich(t)
 
@@ -28,35 +61,13 @@ func Test_BadRequest(t *testing.T) {
 		"TEST_DATA/fixtures",
 	})
 
-	err := c.ExecuteContext(ctx)
+	err = c.ExecuteContext(ctx)
 	if err != nil && a.Log().GetSLog() != nil {
 		a.Log().Error(err.Error())
 		t.Fatal(err)
 	}
 
-	client, err := immich.NewImmichClient(
-		e2e.MyEnv("IMMICHGO_SERVER"),
-		e2e.MyEnv("IMMICHGO_APIKEY"),
-	)
-	ctx2, cancel := context.WithDeadline(ctx, time.Now().Add(30*time.Second))
-check:
-	for {
-		select {
-		case <-ctx2.Done():
-			t.Fatal("Timeout waiting for metadata job to terminate")
-		default:
-			jobs, err := client.GetJobs(ctx2)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if jobs["metadataExtraction"].JobCounts.Active == 0 {
-				cancel()
-				break check
-			}
-			fmt.Println("Waiting for metadata extraction to finish")
-			time.Sleep(1 * time.Second)
-		}
-	}
+	e2e.WaitingForJobsEnding(ctx, client, t)
 
 	c, a = cmd.RootImmichGoCommand(ctx)
 	c.SetArgs([]string{
