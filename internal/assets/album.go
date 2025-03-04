a1b433282aada2bb9a148be0f7cd423f151ffee3
simulot <jfcassan+github@gmail.com>
2025-03-04 16:20:44 +0100

    feat: add asset management methods to Album struct

--- internal/assets/album.go
+++ internal/assets/album.go
@@ -2,14 +2,17 @@ package assets
 
 import (
 	"log/slog"
+
+	"github.com/simulot/immich-go/internal/gen/syncset"
 )
 
 type Album struct {
-	ID          string  `json:"-"`                     // The album ID
-	Title       string  `json:"title,omitempty"`       // either the directory base name, or metadata
-	Description string  `json:"description,omitempty"` // As found in the metadata
-	Latitude    float64 `json:"latitude,omitempty"`    // As found in the metadata
-	Longitude   float64 `json:"longitude,omitempty"`   // As found in the metadata
+	ID          string               `json:"-"`                     // The album ID
+	Title       string               `json:"title,omitempty"`       // either the directory base name, or metadata
+	Description string               `json:"description,omitempty"` // As found in the metadata
+	Latitude    float64              `json:"latitude,omitempty"`    // As found in the metadata
+	Longitude   float64              `json:"longitude,omitempty"`   // As found in the metadata
+	Assets      *syncset.Set[string] `json:"-"`                     // The assets in the album
 }
 
 func (a Album) LogValue() slog.Value {
@@ -19,4 +22,37 @@ func (a Album) LogValue() slog.Value {
 		slog.Float64("latitude", a.Latitude),
 		slog.Float64("longitude", a.Longitude),
 	)
+}
+
+func (a *Album) AddAsset(assetID string) bool {
+	return a.Assets.Add(assetID)
+}
+
+func (a *Album) AddAssets(assetIDs ...string) {
+	for _, assetID := range assetIDs {
+		a.Assets.Add(assetID)
+	}
+}
+
+func (a *Album) RemoveAsset(assetID string) {
+	a.Assets.Remove(assetID)
+}
+
+func (a *Album) ContainsAsset(assetID string) bool {
+	return a.Assets.Contains(assetID)
+}
+
+func (a *Album) AssetIDs() []string {
+	return a.Assets.Items()
+}
+
+func NewAlbum(id string, title string, description string, latitude float64, longitude float64) *Album {
+	return &Album{
+		ID:          id,
+		Title:       title,
+		Description: description,
+		Latitude:    latitude,
+		Longitude:   longitude,
+		Assets:      syncset.NewSet[string](),
+	}
 }
