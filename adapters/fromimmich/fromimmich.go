973289936ec4b192c35501ecc0b4d76f50203367
Simulot <5980377+simulot@users.noreply.github.com>
2025-09-24 10:38:19 +0200

    feat: enable tag filtering in FromImmich by adding Tags field to FromImmichFlags and updating asset retrieval

--- adapters/fromimmich/fromimmich.go
+++ adapters/fromimmich/fromimmich.go
@@ -21,9 +21,9 @@ type FromImmich struct {
 	ifs *immichfs.ImmichFS
 	ic  *filenames.InfoCollector
 
-	albums          []immich.AlbumSimplified
-	mustFetchAlbums bool // When there isn't any album as criteria, should check if the asset belongs to an album (true)
-	errCount        int  // Count the number of errors, to stop after 5
+	albums   []immich.AlbumSimplified
+	tags     []immich.TagSimplified
+	errCount int // Count the number of errors, to stop after 5
 }
 
 func NewFromImmich(ctx context.Context, app *app.Application, jnl *fileevent.Recorder, flags *FromImmichFlags) (*FromImmich, error) {
@@ -64,6 +64,7 @@ const timeFormat = "2006-01-02T15:04:05.000Z"
 
 func (f *FromImmich) getAssets(ctx context.Context, grpChan chan *assets.Group) error {
 	var albumsIDs []string
+	var tagsIds []string
 	var err error
 	client := f.flags.client.Immich
 
@@ -81,62 +82,68 @@ func (f *FromImmich) getAssets(ctx context.Context, grpChan chan *assets.Group) 
 		}
 	}
 
+	if len(f.flags.Tags) > 0 {
+		f.tags, err = client.GetAllTags(ctx)
+		if err != nil {
+			return err
+		}
+		for _, fromTag := range f.tags {
+			for _, t := range f.flags.Tags {
+				if t == fromTag.Value {
+					tagsIds = append(tagsIds, fromTag.ID)
+				}
+			}
+		}
+	}
+
 	query := immich.SearchMetadataQuery{
-		Make:       f.flags.Make,
-		Model:      f.flags.Model,
-		WithExif:   true,
+		Make:  f.flags.Make,
+		Model: f.flags.Model,
+		// WithExif:   true,
 		IsFavorite: f.flags.Favorite,
 		AlbumIds:   albumsIDs,
+		TagIds:     tagsIds,
+
 		// WithArchived: f.flags.WithArchived,
 	}
 
-	f.mustFetchAlbums = true
 	if f.flags.DateRange.IsSet() {
 		query.TakenAfter = f.flags.DateRange.After.Format(timeFormat)
 		query.TakenBefore = f.flags.DateRange.Before.Format(timeFormat)
 	}
 
 	return f.flags.client.Immich.GetAllAssetsWithFilter(ctx, &query, func(a *immich.Asset) error {
-		// apply filters that don't fit in the immch search api
-		if f.flags.MinimalRating > 0 && a.Rating < f.flags.MinimalRating {
-			return nil
-		}
-		if f.flags.DateRange.IsSet() {
-			if !f.flags.DateRange.InRange(a.ExifInfo.DateTimeOriginal.Time) {
-				return nil
-			}
-		}
-
-		asset := a.AsAsset()
-		asset.SetNameInfo(f.ic.GetInfo(asset.OriginalFileName))
-		asset.File = fshelper.FSName(f.ifs, a.ID)
-
-		asset.FromApplication = &assets.Metadata{
-			FileName:    a.OriginalFileName,
-			FileDate:    a.FileModifiedAt.Time,
-			Latitude:    a.ExifInfo.Latitude,
-			Longitude:   a.ExifInfo.Longitude,
-			Description: a.ExifInfo.Description,
-			DateTaken:   a.ExifInfo.DateTimeOriginal.Time,
-			Trashed:     a.IsTrashed,
-			Archived:    a.IsArchived,
-			Favorited:   a.IsFavorite,
-			Rating:      byte(a.Rating),
-			Tags:        asset.Tags,
-		}
-
-		simplifiedA, err := f.flags.client.Immich.GetAssetAlbums(ctx, a.ID)
+		// Fetch details
+		a, err := f.flags.client.Immich.GetAssetInfo(ctx, a.ID)
 		if err != nil {
 			return f.logError(err)
 		}
 
+		// apply filters that don't fit in the immch search api
+		if f.flags.MinimalRating > 0 && a.Rating < f.flags.MinimalRating {
+			return nil
+		}
+
+		asset := a.AsAsset()
+		asset.File = fshelper.FSName(f.ifs, a.ID)
+
+		// Transfer the album
+		simplifiedA, err := f.flags.client.Immich.GetAssetAlbums(ctx, a.ID)
+		if err != nil {
+			return f.logError(err)
+		}
 		albums := immich.AlbumsFromAlbumSimplified(simplifiedA)
 		// clear the ID of the album that exists in from server, but not in to server
 		for i := range albums {
 			albums[i].ID = ""
 		}
-		asset.FromApplication.Albums = albums
+
 		asset.Albums = albums
+
+		// Transfer tags
+		for t := range asset.Tags {
+			asset.Tags[t].ID = ""
+		}
 
 		g := assets.NewGroup(assets.GroupByNone, asset)
 		select {
