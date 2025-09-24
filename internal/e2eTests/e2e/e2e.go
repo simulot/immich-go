5e82327647d26c2cf3b0e5e05d6e429aaffd2d9b
Simulot <5980377+simulot@users.noreply.github.com>
2025-09-24 11:49:13 +0200

    fix: correct SQL command order in ResetImmich function to prevent app setting  deletion

--- internal/e2eTests/e2e/e2e.go
+++ internal/e2eTests/e2e/e2e.go
@@ -51,11 +51,11 @@ func ResetImmich(t *testing.T) {
         delete from asset CASACDE;
         delete from asset_face CASACDE;
         delete from activity CASACDE;
+        delete from tag CASACDE;
         -- delete from api_key CASACDE;
         -- delete from session CASACDE;
         -- delete from user CASACDE;
-        delete from system_metadata where "key" NOT IN ('reverse-geocoding-state', 'system-flags');
-        delete from tag CASACDE;
+        -- delete from system_metadata where "key" NOT IN ('reverse-geocoding-state', 'system-flags');
 		`,
 	)
 	out, err := c.CombinedOutput()
