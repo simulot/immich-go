64a894f1220a69e512623d395258960ef437e6ce
Simulot <5980377+simulot@users.noreply.github.com>
2025-10-02 10:56:33 +0200

    fix(call): capture error in serverCall.do method for improved error handling

--- immich/call.go
+++ immich/call.go
@@ -223,6 +223,7 @@ func (sc *serverCall) do(fnRequest requestFunction, opts ...serverResponseOption
 	resp, err = sc.ic.client.Do(req)
 	// any non nil error must be returned
 	if err != nil {
+		sc.err = err
 		return sc.Err(req, nil, nil)
 	}
 
